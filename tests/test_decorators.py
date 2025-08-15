ae8fe7312d2245744ec4b2865d7353dfd248b24f
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-08-15 23:04:49 +0300

    add test

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,7 +1,6 @@
 import pytest
 
 from src.decorators import log, my_function
-from time import perf_counter
 
 
 def test_log_cupsys(capsys):
@@ -11,10 +10,13 @@ def test_log_cupsys(capsys):
         captured = my_func.readouterr()
         assert captured.out == Exception
 
+
 def test_log():
     """Тестирование декоратора log для функции my_function"""
-    @log(filename='my_log.txt')
+
+    @log(filename="my_log.txt")
     def my_function(x, y):
         return x + y
-    result = my_function(1, 2)
-    assert ' my_function ok. Результат : 3'
+
+    my_function(1, 2)
+    assert " my_function ok. Результат : 3"
