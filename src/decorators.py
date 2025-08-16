15ccd0b45a4a39ab0320eb86151de20656880b8b
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-08-16 05:30:54 +0300

    fix decorators

--- src/decorators.py
+++ src/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 from time import perf_counter
 
 
-def log(filename):
+def log(filename=None):
     """Декоратор log, который автоматически записывает в лог начало и конец выполнения функции,
     а также ее результаты или возникшие ошибки"""
 
@@ -13,29 +13,39 @@ def log(filename):
                 begin = perf_counter()
                 result = func(*args, **kwargs)
                 end_work_func = perf_counter()
-                if filename != "mylog.txt":
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f" {func.__name__} ok. Результат : {result} ")
+
+                else:
                     print(f" {func.__name__} ok. Результат : {result}")
                     # print(f'"Начало работы": {begin}')
                     # print(f'"Конец работы": {end_work_func}')
-                else:
-                    with open("mylog.txt", "w", encoding="utf-8") as file:
-                        file.write(f" {func.__name__} ok. Результат : {result}")
 
             except Exception as e:
-                if filename != "mylog.txt":
-                    print(f"{func.__name__} error: {type(e).__name__}: {e}. Inputs: {args}, {kwargs}")
-                else:
-                    with open("mylog.txt", "w", encoding="utf-8") as file:
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
                         file.write(f"{func.__name__} error: {type(e).__name__}: {e}. Inputs: {args}, {kwargs}")
+
+                else:
+                    print(f"{func.__name__} error: {type(e).__name__}: {e}. Inputs: {args}, {kwargs}")
 
         return wrapper
 
     return inner
 
 
-@log(filename="mylog.txt")
+@log(filename="")
 def my_function(x, y):
     return x + y
 
 
 my_function(1, 2)
+
+
+@log(filename="")
+def my_function_mult(x, y):
+    return x * y
+
+
+my_function_mult(3, 2)
