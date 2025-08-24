3eeb54bf6a512194085b5109d8c0fff524a0b158
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-08-24 12:27:38 +0300

    fixed external

--- src/external_api.py
+++ src/external_api.py
@@ -1,25 +1,47 @@
 import os
 import requests
 from typing import Dict, Any
+from dotenv import load_dotenv
+from src.utils import load_json_file
+
+load_dotenv()
+
+
 def rub_convert_transaction(transaction: Dict[str, Any]) -> float:
     """Функция осуществляет конвертацию суммы транзакции в рубли"""
     try:
-        amount = float(transaction.get('amount', 0))
-        currency = transaction.get('currency', 'RUB').upper()
-        if currency == 'RUB':
-            return amount
-        api_key = os.getenv('API_KEY')
+        api_key = os.getenv("API_KEY")
         if not api_key:
             raise ValueError("API ключ не найден")
-        if currency in ['USD', 'EUR']:
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
-            headers = {"apikey": api_key}
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                data = response.json()
+        amount = float(transaction.get("operationAmount", {}).get("amount", 0))
+        currency = transaction.get("operationAmount", {}).get("currency", {}).get("code", {})
+        to = "RUB"
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={currency}&amount={amount}"
+
+        payload = {}
+        headers = {"apikey": api_key}
+        if currency == "USD" and currency == "EUR":
+            response = requests.request("GET", url, headers=headers, data=payload)
+            status_code = response.status_code
+            result = response.text
+            if status_code == 200:
+                import json
+
+                python_response = json.loads(result)
+                amount = float(python_response.get("result", 0))
+                return amount
             else:
                 print(f"Ошибка API: {response.status_code}")
-                return amount
+
+        else:
+            return amount
+
     except Exception as e:
         print(f"Ошибка при конвертации: {e}")
-        return float(transaction.get('amount', 0))
+
+
+transactions = load_json_file("E:/lessons/homework/data/operations.json")
+for transaction in transactions:
+    amount = rub_convert_transaction(transaction)
+    print(f"Транзакция: {amount} RUB")
--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,26 @@
 import json
 from typing import List, Dict, Any
+
+
 def load_json_file(file_path: str) -> List[Dict[str, Any]]:
-    """ Функция, принимающая путь до JSON-файла и
+    """Функция, принимающая путь до JSON-файла и
     возвращающая список словарей с данными о
     финансовых транзакциях"""
     try:
-        with open(file_path, 'r', encoding='utf-8') as f:
+        with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
                 return data
             else:
                 return []
     except FileNotFoundError:
-        print(f'файл {file_path} не найден')
+        print(f"файл {file_path} не найден")
         return []
     except json.JSONDecoder:
-        print(f'файл {file_path} содержит ytrjhhtrnysq JSON')
+        print(f"файл {file_path} содержит некорректный JSON")
         return []
+
 
 result = load_json_file("E:/lessons/homework/data/operations.json")
 
-#print(result)
-
+print(result)
