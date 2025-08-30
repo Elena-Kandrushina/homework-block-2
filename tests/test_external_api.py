ceba0b94dc7b43a118b961250a859772e9817582
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-08-30 14:20:04 +0300

    a

---
+++ tests/test_external_api.py
@@ -0,0 +1,6 @@
+import os
+from unittest.mock import patch, Mock
+import pytest
+import requests
+from src.external_api import rub_convert_transaction
+
---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+import pytest
+from unittest.mock import mock_open, patch
+import json
+from src.utils import load_json_file
+
