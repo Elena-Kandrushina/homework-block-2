507247ecf9be82877ad60bd3faaabfa05a813da4
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-08-30 18:24:42 +0300

    write logs

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,16 @@
 import os
 import json
 from typing import List, Dict, Any
+import logging
+
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("../logs/utils.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def load_json_file(file_path: str) -> List[Dict[str, Any]]:
@@ -11,14 +21,17 @@ def load_json_file(file_path: str) -> List[Dict[str, Any]]:
         with open(file_path, "r", encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
+                logger.info("Файл с данными существует")
                 return data
             else:
                 return []
     except FileNotFoundError:
         print(f"файл {file_path} не найден")
+        logger.error("Файл отсутствует")
         return []
     except json.JSONDecoder:
         print(f"файл {file_path} содержит некорректный JSON")
+        logger.error("Файл содержит некорректный JSON")
         return []
 
 
