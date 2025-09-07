60543731f4a0ab81835cda1a42009f5b640c5f04
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-09-07 18:44:02 +0300

    fixed masks

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-#import logging
+# import logging
 
 
 # logger = logging.getLogger("masks")
@@ -18,11 +18,11 @@ def get_mask_card_number(card_number: str) -> str:
     card_number = str(card_number)
 
     if len(card_number) == 16:
-        #logger.info("Введен корректный номер")
+        # logger.info("Введен корректный номер")
         mask_card_number = f"{card_number[:4]} {card_number[4:6]} ** **** {card_number[12:]}"
-        #logger.info("Номер маскирован")
+        # logger.info("Номер маскирован")
         return mask_card_number
-    #logger.error("Введен некорректный номер")
+    # logger.error("Введен некорректный номер")
 
     return "Введен некорректный номер"
 
@@ -37,12 +37,12 @@ def get_mask_account(account_number: str) -> str:
 
     account_number = str(account_number)
     if len(account_number) >= 4:
-        #logger.info("Введен корректный номер")
+        # logger.info("Введен корректный номер")
         mask_account_number = f"**{account_number[-4:]}"
-        #logger.info("Номер маскирован")
+        # logger.info("Номер маскирован")
 
         return mask_account_number
-    #logger.error("Введен некорректный номер")
+    # logger.error("Введен некорректный номер")
     return "Введен некорректный номер"
 
 
