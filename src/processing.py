4dcf40778b8d0cab237f25bb5ea662db6998206b
Elena Kandrushina <kandrushina.ea@gmail.com>
2025-09-07 18:45:03 +0300

    fixed pro

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+
+from dateutil import parser
 
 
 def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
@@ -12,35 +13,35 @@ def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
     return new_list_dict
 
 
-if __name__ == "__main__":
-    print(
-        filter_by_state(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
+# if __name__ == "__main__":
+#     print(
+#         filter_by_state(
+#             [
+#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#             ]
+#         )
+#     )
 
 
 def sort_by_date(date_list_dict: list, date_key: str = "date", descending: bool = True) -> list:
     """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать отсортированный по
      дате (date) список """
-    sorted_list_dict = sorted(date_list_dict, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"))
+    sorted_list_dict = sorted(date_list_dict, key=lambda x: parser.isoparse(x["date"]))
     return sorted_list_dict
 
 
-if __name__ == "__main__":
-    print(
-        sort_by_date(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
+# if __name__ == "__main__":
+#     print(
+#         sort_by_date(
+#             [
+#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#             ]
+#         )
+#     )
